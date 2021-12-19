37014d2f8cd79da2078e2962cb90e81bf88e2bbc
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2021-12-19 08:43:56 +0700

    fix wrong path

--- js/Sound.js
+++ js/Sound.js
@@ -17,7 +17,7 @@ export class Sound {
 
 export let beam_sounds = []
 for(let i = 0; i < 15; i++) {
-    let soundTemp = new Sound('./../assets/sounds/alienBeam.wav');
+    let soundTemp = new Sound('./assets/sounds/alienBeam.wav');
     beam_sounds.push(soundTemp);
 }
 
@@ -30,49 +30,49 @@ export function alien_beam() {
 }
 
 export function player_bullet() {
-    let s = new Sound('./../assets/sounds/bulletSound.wav');
+    let s = new Sound('./assets/sounds/bulletSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function game_start() {
-    let s = new Sound('./../assets/sounds/gameStart.wav');
+    let s = new Sound('./assets/sounds/gameStart.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function game_over() {
-    let s = new Sound('./../assets/sounds/deathSound.wav');
+    let s = new Sound('./assets/sounds/deathSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function level_up() {
-    let s = new Sound('./../assets/sounds/levelUpSound.wav');
+    let s = new Sound('./assets/sounds/levelUpSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function boss() {
-    let s = new Sound('./../assets/sounds/bossSound.wav');
+    let s = new Sound('./assets/sounds/bossSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function damage() {
-    let s = new Sound('./../assets/sounds/damageSound.wav');
+    let s = new Sound('./assets/sounds/damageSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function hit() {
-    let s = new Sound('./../assets/sounds/hitmarkerSound.wav');
+    let s = new Sound('./assets/sounds/hitmarkerSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
 
 export function bonus() {
-    let s = new Sound('./../assets/sounds/bonusSound.wav');
+    let s = new Sound('./assets/sounds/bonusSound.wav');
     s.sound.volume = 0.20;
     s.play();
 }
